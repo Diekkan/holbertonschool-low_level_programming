019c92bd8067f8976fba52ffc2a0e59b48c8b69b
Diekkan <merentiel.diego@gmail.com>
2021-09-30 11:02:13 -0700

    a C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line, to thestandard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - main function
  * Return: the value that returns to the OS
  */
 int main(void)
 {
-	fprintf(stderr ,"and that piece of art is useful\"
-- Dora Korpar, 2015-10-19\n");
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }
