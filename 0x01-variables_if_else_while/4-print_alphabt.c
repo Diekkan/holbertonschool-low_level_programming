f75a0ce8b05690845e19266c08cfe5dd0bc2bfd9
Diekkan <merentiel.diego@gmail.com>
2021-10-01 16:36:23 -0700

    a program that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - god function
+ * Return: 0 success value
+ */
+
+int main(void)
+{
+	int lowc;
+	char newline = '\n';
+	lowc = 'a';
+
+	for (lowc = 'a'; lowc <= 'z'; lowc++)
+	{
+	if (lowc != 'e' && lowc != 'q')
+		putchar(lowc);
+	}
+
+	putchar(newline);
+
+	return (0);
+}
