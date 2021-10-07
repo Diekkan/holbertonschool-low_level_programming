b73793eb81f300395e0ca9af45cb59657f403463
Diekkan <merentiel.diego@gmail.com>
2021-10-06 17:36:59 -0700

    comment the part where the infinite loop is done

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,18 +7,18 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
-        i = 0;
+	i = 0;
 
-        while (i < 10)
-        {
-		putchar(i);	// this line of code is wrong
+	while (i < 10)
+	{
+		putchar(i);// this line of code is wrong
 	}
-
-        printf("Infinite loop avoided! \\o/\n");
-
-        return (0);
+	
+	printf("Infinite loop avoided! \\o/\n");
+	
+	return (0);
 }
