3fdfd716aae68c85cb15edf7062e50548d952cbf
Diekkan <merentiel.diego@gmail.com>
2021-10-15 10:03:05 -0700

    update 2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcat - a function that concatenates two strings
+ * @dest: dest string
+ * @src: source string
+ * Return: dest string modified
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+        int chs1;
+        int chs2;
+
+        for (chs1 = 0; dest[chs1];)
+	{
+		chs1++;
+	}
+
+	for (chs2 = 0; src[chs2] < n && src[chs2] != '\0';)
+	{
+		dest[chs1] = src[chs2];
+		chs2++;
+		chs1++;
+	}
+
+	dest[chs1] = '\0';
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int chs1;
+	int chs2;
+	
+	for (; src <= n;)
+	{
+		chs1++;
+        }
+
+	if (chs1 <= n)
+	{
+
+	}
+	
+	
