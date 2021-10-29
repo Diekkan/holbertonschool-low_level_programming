d60c8176a5a12a44b30129788060eaf43e8f4dfa
Diekkan <merentiel.diego@gmail.com>
2021-10-28 17:32:55 -0700

    a function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,58 +1,40 @@
 #include <stdlib.h>
 #include "main.h"
+
 /**
- *
+ * string_nconcat - concatenates two strings.
+ *@s1:
+ *@s2:
+ *@n:
+ *Return:
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0, m, lend;
+	unsigned int i = 0, j = 0, m , lend;
 	char *p;
 
-	while (s1[i])
+	while(s1[i])
 		i++;
-	while (s2[j])
+    	while(s2[j])
 		j++;
-
-	if (i + j == 0)
-		return(NULL);
-	if (n == 0)
-	{	
-		p = (char *) malloc(sizeof(char) * (i + 1));
-		p = s1;
-		return (p);
-	}
-	if (n < j)
-	{
-		lend = j - n;
-		p = (char *) malloc(sizeof(char) * (i + lend) + 1);
-		if (i == 0)
-		{
-			for (j = 0; j <= lend; j++)
-				p[j] = s2[j];
-			p[(i + lend) + 2] = '\0';
-			return (p);
-		}			
-		else
-		{
-			for (j = 0; i >= j; j++)
-			{
-				p[j] = s1[j];
-			}
-			for (m = 0; m <= (lend + 1); m++)
-				p[(m + i)] = s2[m];
-			p[(i + lend) + 2] = '\0';
-			return (p);
-		}
-	}
+	
+	if (n > 0)
+	    lend = j - n;
+	else if (n >= j)
+		lend = j;
 	else
-	{
-		 p = (char *) malloc(sizeof(char) * (i + j));
-		 for (j = 0; i >= j; j++)
-                                p[j] = s1[j];
-		 for (m = 0; m <= j; m++)
-                                p[m + i] = s2[m];
-		 p[(i + j) + 1] = '\0';
-                        return (p);
-	}
+		lend = 0;
+
+	p = malloc(sizeof(char) * (i + lend + 1));
+
+	for (m = 0; m <= i; m++)
+		p[m] = s1[m];
+
+	for(m = 0; m <= (lend + 1); m++)
+		p[m + i] = s2[m];
+
+	p[i + lend + 2] = 0;
+	
+	return(p);
 }
