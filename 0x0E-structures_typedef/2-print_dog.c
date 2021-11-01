a2b4c96fdd5d9c0e762e08927f6981ccf0f4acc5
Diekkan <merentiel.diego@gmail.com>
2021-11-01 11:10:41 -0700

    a function that prints a struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,41 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - a function that prints a struct dog.
+ * @d: pointer to structure.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name!= NULL)
+		{
+			printf("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf("Name: (nil)\n");
+		}
+
+		if (d->age != '\0')
+		{
+			printf("Age: %f\n", d->age);
+		}
+		else
+		{
+			printf("Age: (nil)\n");
+		}
+
+		if (d->owner != NULL)
+		{
+			printf("Name: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Name: (nil)\n");
+		}
+	}
+}
