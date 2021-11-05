fa28e388c18203bd2af3c1e428d8adf4218549ad
Diekkan <merentiel.diego@gmail.com>
2021-11-05 05:57:18 -0700

    new

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -18,7 +18,7 @@ void print_all(const char * const format, ...)
 	va_start(ap, format);
 	separator = "";
 
-	while (format[a] && format)
+	while (format && format[a])
 	{
 		switch (format[a])
 		{
