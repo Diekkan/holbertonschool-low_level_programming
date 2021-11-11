3956340f7dfb363e5bbe6fc7a343546a4dc42327
Diekkan <merentiel.diego@gmail.com>
2021-11-11 07:37:35 -0800

    main library and prototypes of the project

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct list_t - list for example
+ *@str: string
+ *@len:lenght of the str
+ *@next: pointer to next node
+ */
 typedef struct list_t
 {
 	char *str;
