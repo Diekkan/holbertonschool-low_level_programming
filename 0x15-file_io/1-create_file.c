18863a1a7c8a5bf4dbdcc1c7148ccfcdeff072b8
Diekkan <merentiel.diego@gmail.com>
2021-11-22 07:41:41 -0800

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,6 +26,12 @@ int create_file(const char *filename, char *text_content)
 		if (wr == -1)
 			return (-1);
 	}
+	else
+	{
+		wr = write(fd, '\0', 1);
+		if (wr == -1)
+			return (-1);
+	}
 
 	return (1);
 }
