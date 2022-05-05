2d14c1d3f538a2993b6a348d3a146ca18d3a5dfa
Diekkan <merentiel.diego@gmail.com>
2022-05-04 19:50:56 -0700

    Binary search algorithm incomplete

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - makes a binary search
+ *@array: array to look
+ *@size: no of elements in the array
+ *@value: value to look for
+ *Return: returns -1 if not found, otherwise, index no.
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+}
